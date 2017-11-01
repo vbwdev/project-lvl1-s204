d35de9da82906ed23b0f9d6cc9e96acf821f03c1
Sergey Nikolsky <vbwdev@gmail.com>
2017-11-01 15:57:24 +0300

    Add brain-even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+import { greeting, acquaintance, gameBrainEven } from '..';
+
+greeting();
+const userName = acquaintance();
+gameBrainEven(userName);
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import greeting from '..';
+import { greeting, acquaintance } from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log(greeting());
+greeting();
+acquaintance();
