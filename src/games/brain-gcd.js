c864faf766c4d702fd6d7c813e9896e104a9e06e
Sergey Nikolsky <vbwdev@gmail.com>
2017-11-03 00:54:24 +0300

    Add brain-gcd game

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,38 @@
+import makeGame from '..';
+
+
+const getRandomNum = (min, max) => min + Math.round(Math.random() * (1 + max + -min));
+
+
+const checkGCD = (num1, num2, gcd) => {
+  if (num2 > 0) {
+    return checkGCD(num2, num1 % num2, gcd);
+  }
+
+  return Math.abs(num1) === gcd;
+};
+
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+
+const questionGenerator = () => {
+  const gcd = getRandomNum(1, 10);
+  const num1 = gcd * getRandomNum(1, 10);
+  const num2 = gcd * getRandomNum(1, 10);
+
+  // Иногда одно из чисел становится НОД сгенерированной пары,
+  // поэтому проверяем, остался ли сгенерированный НОД верным
+  const isCorrectGCD = checkGCD(num1, num2, gcd);
+  if (num1 === num2 || !isCorrectGCD) {
+    return questionGenerator();
+  }
+
+  return [`${num1} ${num2}`, gcd];
+};
+
+
+const answerChecker = (userAnswer, correctAnswer) => parseInt(userAnswer, 10) === correctAnswer;
+
+
+export default makeGame(gameDescription, questionGenerator, answerChecker);
