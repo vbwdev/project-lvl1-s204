9ce4bc019793030ed212b9a94bbb92e7002124a4
Sergey Nikolsky <vbwdev@gmail.com>
2017-11-04 00:31:35 +0300

    Add brain-progression game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,12 +1,26 @@
 import makeGame, { getRandomNum } from '..';
 
 
+const PROGRESSION_LENGTH = 10;
+
 const gameDescription = 'What number is missing in this progression?';
 
 const questionGenerator = () => {
-  const num = getRandomNum(100, 1000);
-  const answer = '';
-  return [num, answer];
+  const firstNum = getRandomNum(1, 100);
+  const step = getRandomNum(1, 20);
+
+  const makeProgression = (length, acc = [firstNum]) => {
+    if (length === 0) {
+      return acc;
+    }
+    return makeProgression(length - 1, [...acc, acc[acc.length - 1] + step]);
+  };
+
+  const progression = makeProgression(PROGRESSION_LENGTH);
+  const answer = progression[getRandomNum(0, PROGRESSION_LENGTH)];
+  const question = progression.join(' ').replace(answer, '..');
+
+  return [question, answer];
 };
 
 export default makeGame(gameDescription, questionGenerator);
