8e4005f1c9625cccb8aef2d8de08e8d9683c9756
Sergey Nikolsky <vbwdev@gmail.com>
2017-11-03 13:38:22 +0300

    Rework game iter – remove data repeating

--- src/index.js
+++ src/index.js
@@ -11,44 +11,34 @@ export const acquaintance = () => {
   return userName;
 };
 
-const gameIter = (
-  questionGenerator,
-  answerChecker,
-  correctAnswers = 0,
-  correctAnswersToWin = CORRECT_ANSWERS_TO_WIN,
-) => {
-  if (correctAnswers === correctAnswersToWin) {
-    return true;
-  }
-
-  const [question, correctAnswer] = questionGenerator();
-  console.log(`Question: ${question}`);
-
-  const userAnswer = readlineSync.question('Your answer: ');
-  const isCorrectAnswer = answerChecker(userAnswer, correctAnswer);
-
-  if (isCorrectAnswer) {
-    console.log('Correct!');
-    return gameIter(
-      questionGenerator,
-      answerChecker,
-      correctAnswers + 1,
-      correctAnswersToWin,
-    );
-  }
-
-  console.log(`"${userAnswer}" is wrong answer :( Correct answer was "${correctAnswer}".`);
-  return false;
-};
-
 const makeGame = (gameDescription, questionGenerator, answerChecker) => () => {
+  const gameIter = (correctAnswers) => {
+    if (correctAnswers === CORRECT_ANSWERS_TO_WIN) {
+      return true;
+    }
+
+    const [question, correctAnswer] = questionGenerator();
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isCorrectAnswer = answerChecker(userAnswer, correctAnswer);
+
+    if (isCorrectAnswer) {
+      console.log('Correct!');
+      return gameIter(correctAnswers + 1);
+    }
+
+    console.log(`"${userAnswer}" is wrong answer :( Correct answer was "${correctAnswer}".`);
+    return false;
+  };
+
   greeting();
 
   console.log(`${gameDescription}\n`);
 
   const userName = acquaintance();
 
-  const isVictory = gameIter(questionGenerator, answerChecker);
+  const isVictory = gameIter(0);
 
   if (isVictory) {
     console.log(`Congratulations, ${userName}!`);
